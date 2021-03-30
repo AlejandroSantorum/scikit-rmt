e7afa555e76dc20b9fbc79ea4a5bc288cf22357d
Alejandro Santorum <alejandro.santorum@gmail.com>
2021-03-30 23:36:23 +0200

    deleted private methods to sample

--- rmtpy/ensemble/gaussian_ensemble.py
+++ rmtpy/ensemble/gaussian_ensemble.py
@@ -25,65 +25,56 @@ class GaussianEnsemble(_Ensemble):
 ##########################################
 ### Gaussian Orthogonal Ensemble = GOE
 
-def _sample_goe(n):
-        # n by n matrix of random Gaussians
-        A = np.random.randn(n,n)
-        # symmetrize matrix
-        return (A + A.transpose())/2
-
 class GOE(GaussianEnsemble):
 
     def __init__(self, n):
         super().__init__(beta=1)
-        self.matrix = _sample_goe(n)
+        self.matrix = self.sample(n)
 
     def sample(self, n):
-        self.matrix = self._sample_goe(n)
+        # n by n matrix of random Gaussians
+        A = np.random.randn(n,n)
+        # symmetrize matrix
+        self.matrix = (A + A.transpose())/2
         return self.matrix
 
 
 ##########################################
 ### Gaussian UNITARY Ensemble = GUE
 
-def _sample_gue(n):
-        # n by n random complex matrix
-        A = np.random.randn(n,n) + (0+1j)*np.random.randn(n,n)
-        # hermitian matrix
-        return (A + A.transpose())/2
-
 class GUE(GaussianEnsemble):
 
     def __init__(self, n):
         super().__init__(beta=2)
-        self.matrix = _sample_gue(n)
+        self.matrix = self.sample(n)
 
     def sample(self, n):
-        self.matrix = _sample_gue(n)
+        # n by n random complex matrix
+        A = np.random.randn(n,n) + (0+1j)*np.random.randn(n,n)
+        # hermitian matrix
+        self.matrix = (A + A.transpose())/2
         return self.matrix
 
 
 ##########################################
 ### Gaussian Orthogonal Ensemble = GOE
 
-def _sample_gse(n):
-    # n by n random complex matrix
-    X = np.random.randn(n,n) + (0+1j)*np.random.randn(n,n)
-    # another n by n random complex matrix
-    Y = np.random.randn(n,n) + (0+1j)*np.random.randn(n,n)
-    # [X Y; -conj(Y) conj(X)] 
-    A = np.block([
-                    [X               , Y],
-                    [-np.conjugate(Y), np.conjugate(X)]
-                    ])
-    # hermitian matrix
-    return (A + A.transpose())/2
-
 class GSE(GaussianEnsemble):
 
     def __init__(self, n):
         super().__init__(beta=4)
-        self.matrix = _sample_gse(n)
+        self.matrix = self.sample(n)
 
     def sample(self, n):
-        self.matrix = _sample_gse(n)
+        # n by n random complex matrix
+        X = np.random.randn(n,n) + (0+1j)*np.random.randn(n,n)
+        # another n by n random complex matrix
+        Y = np.random.randn(n,n) + (0+1j)*np.random.randn(n,n)
+        # [X Y; -conj(Y) conj(X)] 
+        A = np.block([
+                        [X               , Y],
+                        [-np.conjugate(Y), np.conjugate(X)]
+                        ])
+        # hermitian matrix
+        self.matrix = (A + A.transpose())/2
         return self.matrix
