3fdd2e0fa75a8da2749645e67ed5332e9716603c
Alejandro Santorum <alejandro.santorum@gmail.com>
2021-04-18 15:36:54 +0200

    Deleted wrong implementation

--- skrmt/covariance/analytical_shrinkage.py
+++ skrmt/covariance/analytical_shrinkage.py
@@ -3,68 +3,8 @@ from numpy.matlib import repmat
 
 class AnalyticalShrinkage:
 
-
-    def estimate(self, X, k=None):
-        n, p = X.shape
-        if k is None:
-            # demean data matrix
-            X = X - X.mean(axis=0)
-            # subtract one degree of freedom 
-            k=1
-        # effective sample size
-        n=n-k
-
-        # get sample eigenvalues and eigenvectors, and sort them in ascending order
-        sample = np.matmul(X.T, X)/n
-        eigvals, eigvects = np.linalg.eig(sample)
-        order = np.argsort(eigvals)
-        eigvals = eigvals[order]
-        eigvects = eigvects[order]
-
-        # choose the global bandwidth
-        h = n**(-1/3)
-
-        # estimate the spectral density with the Epanechnikov kernel
-        #### rep_eigvals = np.repeat(eigvals, p)
-        #### H = h*rep_eigvals
-
-        # specify the locally adaptive bandwidth
-        H = h * eigvals
-
-        # estimate the spectral density with the Epanechnikov kernel
-        f_tilde = np.zeros(p)
-        for i in range(p):
-            for j in range(p):
-                second_factor = 1 - 0.2*((eigvals[i] - eigvals[j])/H[j])**2
-                f_tilde[i] += 3/(4*np.sqrt(5)*H[j]) * max(second_factor, 0)
-            f_tilde[i] /= p
-
-        # estimate the Hilbert transform
-        Hilbert = np.zeros(p)
-        for i in range(p):
-            for j in range(p):
-                sum1 = -3*(eigvals[i]-eigvals[j])/(10*np.pi*H[j]**2)
-                factor1 = 3/(4*np.sqrt(5)*np.pi*H[j])*(1 - 0.2*((eigvals[i]-eigvals[j])/H[j])**2)
-                factor2 = np.log(np.abs((np.sqrt(5)*H[j]-eigvals[i]+eigvals[j])/(np.sqrt(5)*H[j]+eigvals[i]-eigvals[j])))
-                Hilbert[i] += (sum1 + factor1*factor2)
-            Hilbert[i] /= p
-        
-        print(Hilbert[-1])
-
-        # compute the asymptotically optimal nonlinear shrinkage
-        d_tilde = np.zeros(p)
-        for i in range(p):
-            den = (np.pi*(p/n)*eigvals[i]*f_tilde[i])**2 + (1-(p/n)-np.pi*(p/n)*eigvals[i]*Hilbert[i])**2
-            d_tilde[i] = eigvals[i]/den
-        
-        S_tilde = np.zeros((p,p))
-        for i in range(p):
-            S_tilde += d_tilde[i] * np.matmul(eigvects[:,i].T, eigvects[:,i].T)
-        
-        return S_tilde
-
     
-    def estimate_vec(self, X, k=None):
+    def estimate(self, X, k=None):
         n, p = X.shape
 
         if k is None:
@@ -114,7 +54,7 @@ def test1():
               [0, 0, 1], [0, 0, 1], [0, 0, 1], [0, 0, 1], [0, 0, 2]])
 
     analsh = AnalyticalShrinkage()
-    sigma_tilde = analsh.estimate_vec(X)
+    sigma_tilde = analsh.estimate(X)
 
     print(sigma_tilde)
 
