269e29ac79df6fedac091731671b94cfca93d06d
Alejandro Santorum <alejandro.santorum@gmail.com>
2021-04-18 19:13:46 +0200

    Add loss_MV and PRIAL metrics (not tested)

--- skrmt/covariance/__init__.py
+++ skrmt/covariance/__init__.py
@@ -4,7 +4,10 @@ to estimate covariance matrices.
 """
 
 from .analytical_shrinkage import AnalyticalShrinkage
-from .estimator import SampleEstimator
+from .estimator import SampleEstimator, FSOptEstimator
+
+from .metrics import loss_mv, loss_frobenius
 
 
-__all__ = ["AnalyticalShrinkage", "SampleEstimator",]
+__all__ = ["AnalyticalShrinkage", "SampleEstimator", "FSOptEstimator",
+           "loss_mv", "loss_frobenius"]
---
+++ skrmt/covariance/metrics.py
@@ -0,0 +1,45 @@
+import numpy as np
+
+from .estimator import SampleEstimator, FSOptEstimator
+
+
+def loss_mv(Sigma_tilde, Sigma):
+    p = len(Sigma)
+
+    try:
+        Sigma_tilde_inv = np.linalg.inv(Sigma_tilde)
+    except np.linalg.LinAlgError:
+        raise ValueError("Unable to invert estimated covariance matrix")
+    
+    try:
+        Sigma_inv = np.linalg.inv(Sigma)
+    except np.linalg.LinAlgError:
+        raise ValueError("Unable to invert population covariance matrix")
+    
+
+    M = np.matmul(np.matmul(Sigma_tilde_inv, Sigma), Sigma_tilde_inv)
+    loss = np.trace(M)/p/(np.trace(Sigma_tilde_inv)/p)**2 - 1/np.trace(Sigma_inv)/p
+
+    return loss
+
+
+def loss_frobenius(Sigma_tilde, Sigma):
+    p = len(Sigma)
+    loss = np.trace(Sigma_tilde - Sigma)**2/p
+
+    return loss
+
+
+# Percentage relative improvement in average loss
+def PRIAL_mv(Sigma_tilde, Sigma, X):
+    sample_est = SampleEstimator()
+    S = sample_est.estimate(X)
+
+    fsopt = FSOptEstimator(Sigma)
+    S_star = fsopt.estimate(X)
+
+    loss_S_Sigma = loss_mv(S, Sigma)
+    prial = (loss_S_Sigma - loss_mv(Sigma_tilde, Sigma))/(loss_S_Sigma - loss_mv(S_star, Sigma))
+
+    return prial
+
