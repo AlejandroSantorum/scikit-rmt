7cbf048fa4a1d435654b72dce7e3d5045336e992
Alejandro Santorum <alejandro.santorum@gmail.com>
2021-04-20 12:15:00 +0200

    Implemented and tested linear shrinkage estimator

--- skrmt/covariance/__init__.py
+++ skrmt/covariance/__init__.py
@@ -4,10 +4,12 @@ to estimate covariance matrices.
 """
 
 from .analytical_shrinkage import AnalyticalShrinkage
+from .linear_shrinkage import LinearShrinkage
 from .estimator import SampleEstimator, FSOptEstimator
 
 from .metrics import loss_mv, loss_frobenius
 
 
-__all__ = ["AnalyticalShrinkage", "SampleEstimator", "FSOptEstimator",
+__all__ = ["AnalyticalShrinkage", "LinearShrinkage",
+           "SampleEstimator", "FSOptEstimator",
            "loss_mv", "loss_frobenius"]
