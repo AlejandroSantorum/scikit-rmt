00c51921629624febe3de3b3c56fe0a13e6ac740
Alejandro Santorum <alejandro.santorum@gmail.com>
2021-04-29 00:18:19 +0200

    Pylint covariance module

--- skrmt/covariance/tests/test_estimator.py
+++ skrmt/covariance/tests/test_estimator.py
@@ -1,10 +1,11 @@
-import pytest
+'''Estimator Test module
+
+Testing Estimator module
+'''
 
 import numpy as np
 from numpy.testing import (
     assert_almost_equal,
-    assert_array_equal,
-    assert_allclose,
 )
 
 from skrmt.covariance import sample_estimator
@@ -17,18 +18,18 @@ from skrmt.covariance import minimax_estimator
 ################################################################
 # SAMPLE ESTIMATOR 
 
-def test_sampleEstimator():
+def test_sample_estimator():
+    '''Testing sample estimator
+    '''
     # input data matrix
-    X = np.array([[2, 0, 0], [2, 0, 0], [2, 0, 0], [1, 0, 0], [1, 0, 0],
-                  [0, 1, 0], [0, 1, 0], [0, 1, 0], [0, 1, 0], [0, 1, 0],
-                  [0, 0, 1], [0, 0, 1], [0, 0, 1], [0, 0, 1], [0, 0, 2]])
+    mtx = np.array([[2, 0, 0], [2, 0, 0], [2, 0, 0], [1, 0, 0], [1, 0, 0],
+                    [0, 1, 0], [0, 1, 0], [0, 1, 0], [0, 1, 0], [0, 1, 0],
+                    [0, 0, 1], [0, 0, 1], [0, 0, 1], [0, 0, 1], [0, 0, 2]])
 
-    # sample_est = SampleEstimator()
-    # sigma_sample = sample_est.estimate(X)
-    sigma_sample = sample_estimator(X)
+    sigma_sample = sample_estimator(mtx)
 
-    X = X - X.mean(axis=0)
-    numpy_sol = np.cov(X, rowvar=False)
+    mtx = mtx - mtx.mean(axis=0)
+    numpy_sol = np.cov(mtx, rowvar=False)
 
     assert_almost_equal(sigma_sample, numpy_sol, decimal=10)
 
@@ -37,34 +38,38 @@ def test_sampleEstimator():
 ################################################################
 # LINEAR ESTIMATOR 
 
-def test_linearEstimator1():
+def test_linear_estimator1():
+    '''Testing linear estimator (part 1)
+    '''
     # input data matrix
-    X = np.array([[2, 0, 0], [2, 0, 0], [2, 0, 0], [1, 0, 0], [1, 0, 0],
-                  [0, 1, 0], [0, 1, 0], [0, 1, 0], [0, 1, 0], [0, 1, 0],
-                  [0, 0, 1], [0, 0, 1], [0, 0, 1], [0, 0, 1], [0, 0, 2]])
+    mtx = np.array([[2, 0, 0], [2, 0, 0], [2, 0, 0], [1, 0, 0], [1, 0, 0],
+                    [0, 1, 0], [0, 1, 0], [0, 1, 0], [0, 1, 0], [0, 1, 0],
+                    [0, 0, 1], [0, 0, 1], [0, 0, 1], [0, 0, 1], [0, 0, 2]])
 
     # solution using official matlab code (Ledoit and Wolf)
     sol = np.array([[0.571596585433017, -0.119074762494837, -0.142889714993804],
                     [-0.119074762494837, 0.285817155445408, -0.089306071871128],
                     [-0.142889714993804, -0.089306071871128, 0.387030703566020]])
 
-    sigma_tilde = linear_shrinkage_estimator(X)
+    sigma_tilde = linear_shrinkage_estimator(mtx)
 
     assert_almost_equal(sigma_tilde, sol, decimal=10)
 
 
-def test_linearEstimator2():
+def test_linear_estimator2():
+    '''Testing linear estimator (part 2)
+    '''
     # input data matrix
-    X = np.array([[5, 0, 0], [4, 0, 0], [3, 0, 0], [2, 0, 0], [1, 0, 0],
-                  [0, 0, 1], [0, 0, 2], [0, 0, 3], [0, 0, 4], [0, 0, 5],
-                  [0, 3, 0], [0, 4, 0], [0, 5, 0], [0, 1, 0], [0, 10, 0]])
+    mtx = np.array([[5, 0, 0], [4, 0, 0], [3, 0, 0], [2, 0, 0], [1, 0, 0],
+                    [0, 0, 1], [0, 0, 2], [0, 0, 3], [0, 0, 4], [0, 0, 5],
+                    [0, 3, 0], [0, 4, 0], [0, 5, 0], [0, 1, 0], [0, 10, 0]])
 
     # solution using official matlab code (Ledoit and Wolf)
     sol = np.array([[4.157119427352015, -0.175394418448355, -0.114387664205449],
                     [-0.175394418448355, 4.734650034184860, -0.175394418448355],
                     [-0.114387664205449, -0.175394418448355, 4.157119427352015]])
 
-    sigma_tilde = linear_shrinkage_estimator(X)
+    sigma_tilde = linear_shrinkage_estimator(mtx)
 
     assert_almost_equal(sigma_tilde, sol, decimal=10)
 
@@ -72,34 +77,38 @@ def test_linearEstimator2():
 ################################################################
 # ANALYTICAL ESTIMATOR 
 
-def test_analyticalEstimator1():
+def test_analytical_estimator1():
+    '''Testing analytical estimator (part 1)
+    '''
     # input data matrix
-    X = np.array([[2, 0, 0], [2, 0, 0], [2, 0, 0], [1, 0, 0], [1, 0, 0],
-              [0, 1, 0], [0, 1, 0], [0, 1, 0], [0, 1, 0], [0, 1, 0],
-              [0, 0, 1], [0, 0, 1], [0, 0, 1], [0, 0, 1], [0, 0, 2]])
+    mtx = np.array([[2, 0, 0], [2, 0, 0], [2, 0, 0], [1, 0, 0], [1, 0, 0],
+                    [0, 1, 0], [0, 1, 0], [0, 1, 0], [0, 1, 0], [0, 1, 0],
+                    [0, 0, 1], [0, 0, 1], [0, 0, 1], [0, 0, 1], [0, 0, 2]])
 
     # solution using official matlab code (Ledoit and Wolf)
     sol = np.array([[0.551752555904758, -0.176171945854672, -0.127673277267705],
                     [-0.176171945854672, 0.317339265808824, -0.236795281588382],
                     [-0.127673277267705, -0.236795281588382, 0.474063174598454]])
 
-    sigma_tilde = analytical_shrinkage_estimator(X)
+    sigma_tilde = analytical_shrinkage_estimator(mtx)
 
     assert_almost_equal(sigma_tilde, sol, decimal=7)
 
 
-def test_analyticalEstimator2():
+def test_analytical_estimator2():
+    '''Testing analytical estimator (part 2)
+    '''
     # input data matrix
-    X = np.array([[5, 0, 0], [4, 0, 0], [3, 0, 0], [2, 0, 0], [1, 0, 0],
-                  [0, 0, 1], [0, 0, 2], [0, 0, 3], [0, 0, 4], [0, 0, 5],
-                  [0, 3, 0], [0, 4, 0], [0, 5, 0], [0, 1, 0], [0, 10, 0]])
+    mtx = np.array([[5, 0, 0], [4, 0, 0], [3, 0, 0], [2, 0, 0], [1, 0, 0],
+                    [0, 0, 1], [0, 0, 2], [0, 0, 3], [0, 0, 4], [0, 0, 5],
+                    [0, 3, 0], [0, 4, 0], [0, 5, 0], [0, 1, 0], [0, 10, 0]])
 
     # solution using official matlab code (Ledoit and Wolf)
     sol = np.array([[3.84630121492766, -1.3927903545004, -1.42093779975966],
                     [-1.3927903545004, 7.91982043683479, -1.3927903545004],
                     [-1.42093779975966, -1.3927903545004, 3.84630121492766]])
 
-    sigma_tilde = analytical_shrinkage_estimator(X)
+    sigma_tilde = analytical_shrinkage_estimator(mtx)
 
     assert_almost_equal(sigma_tilde, sol, decimal=7)
 
@@ -107,50 +116,56 @@ def test_analyticalEstimator2():
 ################################################################
 # FSOPT ESTIMATOR 
 
-def test_fsoptEstimator_shape_symm():
+def test_fsopt_estimator_shape_symm():
+    '''Testing fsopt estimator: shape & symmetric
+    '''
     # population covariance matrix
-    Sigma = np.array([[3.00407916, -1.46190757, 1.50140806, 1.50933526, 0.27036442],
+    sigma = np.array([[3.00407916, -1.46190757, 1.50140806, 1.50933526, 0.27036442],
                       [-1.46190757, 5.61441061, -0.51939653, -2.76492235, 1.38225566],
                       [1.50140806, -0.51939653, 2.3068582, 1.41248896, 0.84740175],
                       [1.50933526, -2.76492235, 1.41248896, 6.57182938, 0.73407095],
                       [0.27036442, 1.38225566, 0.84740175, 0.73407095, 9.50282265]])
-    
-    p = Sigma.shape[0]
-    n = 12
-    # input data matrix
-    X = np.random.multivariate_normal(np.random.randn(p), Sigma, size=n)
-    
-    sigma_tilde = fsopt_estimator(X, Sigma)
 
-    assert(sigma_tilde.shape == (X.shape[1], X.shape[1]))
-    assert((sigma_tilde.all() == sigma_tilde.all()) == True)
+    p_size = sigma.shape[0]
+    n_size = 12
+    # input data matrix
+    mtx = np.random.multivariate_normal(np.random.randn(p_size), sigma, size=n_size)
+
+    sigma_tilde = fsopt_estimator(mtx, sigma)
+
+    assert sigma_tilde.shape == (mtx.shape[1], mtx.shape[1])
+    assert sigma_tilde.all() == sigma_tilde.all()
 
 
 ################################################################
 # EMPIRICAL BAYESIAN ESTIMATOR 
 
-def test_EBEstimator_shape_symm():
+def test_eb_stimator_shape_symm():
+    '''Testing empirical bayesian estimator: shape & symmetric
+    '''
     # input data matrix
-    X = np.array([[5, 0, 0], [4, 0, 0], [3, 0, 0], [2, 0, 0], [1, 0, 0],
-                  [0, 0, 1], [0, 0, 2], [0, 0, 3], [0, 0, 4], [0, 0, 5],
-                  [0, 3, 0], [0, 4, 0], [0, 5, 0], [0, 1, 0], [0, 10, 0]])
-    
-    sigma_tilde = empirical_bayesian_estimator(X)
+    mtx = np.array([[5, 0, 0], [4, 0, 0], [3, 0, 0], [2, 0, 0], [1, 0, 0],
+                    [0, 0, 1], [0, 0, 2], [0, 0, 3], [0, 0, 4], [0, 0, 5],
+                    [0, 3, 0], [0, 4, 0], [0, 5, 0], [0, 1, 0], [0, 10, 0]])
 
-    assert(sigma_tilde.shape == (X.shape[1], X.shape[1]))
-    assert((sigma_tilde.all() == sigma_tilde.all()) == True)
+    sigma_tilde = empirical_bayesian_estimator(mtx)
+
+    assert sigma_tilde.shape == (mtx.shape[1], mtx.shape[1])
+    assert sigma_tilde.all() == sigma_tilde.all()
 
 
 ################################################################
 # MINIMAX ESTIMATOR 
 
-def test_minimaxEstimator_shape_symm():
+def test_minimax_estimator_shape_symm():
+    '''Testing minimax: shape & symmetric
+    '''
     # input data matrix
-    X = np.array([[5, 0, 0], [4, 0, 0], [3, 0, 0], [2, 0, 0], [1, 0, 0],
-                  [0, 0, 1], [0, 0, 2], [0, 0, 3], [0, 0, 4], [0, 0, 5],
-                  [0, 3, 0], [0, 4, 0], [0, 5, 0], [0, 1, 0], [0, 10, 0]])
-    
-    sigma_tilde = minimax_estimator(X)
+    mtx = np.array([[5, 0, 0], [4, 0, 0], [3, 0, 0], [2, 0, 0], [1, 0, 0],
+                    [0, 0, 1], [0, 0, 2], [0, 0, 3], [0, 0, 4], [0, 0, 5],
+                    [0, 3, 0], [0, 4, 0], [0, 5, 0], [0, 1, 0], [0, 10, 0]])
 
-    assert(sigma_tilde.shape == (X.shape[1], X.shape[1]))
-    assert((sigma_tilde.all() == sigma_tilde.all()) == True)
+    sigma_tilde = minimax_estimator(mtx)
+
+    assert sigma_tilde.shape == (mtx.shape[1], mtx.shape[1])
+    assert sigma_tilde.all() == sigma_tilde.all()
--- skrmt/covariance/tests/test_metrics.py
+++ skrmt/covariance/tests/test_metrics.py
@@ -1,11 +1,9 @@
-import pytest
+'''Metrics Test module
+
+Testing Metrics module
+'''
 
 import numpy as np
-from numpy.testing import (
-    assert_almost_equal,
-    assert_array_equal,
-    assert_allclose,
-)
 
 from skrmt.covariance import sample_estimator
 from skrmt.covariance import fsopt_estimator
@@ -14,48 +12,52 @@ from skrmt.covariance import loss_mv, prial_mv
 
 
 def test_prial_sample():
+    '''Testing prial evaluated in sample covariance matrix
+    '''
     # population covariance matrix
-    Sigma = np.array([[3.00407916, -1.46190757, 1.50140806, 1.50933526, 0.27036442],
+    sigma = np.array([[3.00407916, -1.46190757, 1.50140806, 1.50933526, 0.27036442],
                       [-1.46190757, 5.61441061, -0.51939653, -2.76492235, 1.38225566],
                       [1.50140806, -0.51939653, 2.3068582, 1.41248896, 0.84740175],
                       [1.50933526, -2.76492235, 1.41248896, 6.57182938, 0.73407095],
                       [0.27036442, 1.38225566, 0.84740175, 0.73407095, 9.50282265]])
 
-    p, n = Sigma.shape[0], 3*Sigma.shape[0]
+    p_size, n_size = sigma.shape[0], 3*sigma.shape[0]
     # input data matrix
-    X = np.random.multivariate_normal(np.random.randn(p), Sigma, size=n)
+    mtx = np.random.multivariate_normal(np.random.randn(p_size), sigma, size=n_size)
 
-    sigma_sample = sample_estimator(X)
-    sigma_fsopt = fsopt_estimator(X, Sigma)
+    sigma_sample = sample_estimator(mtx)
+    sigma_fsopt = fsopt_estimator(mtx, sigma)
 
-    E_Sn = loss_mv(sigma_tilde=sigma_sample, sigma=Sigma)
-    E_Sigma_tilde = loss_mv(sigma_tilde=sigma_sample, sigma=Sigma)
-    E_Sstar = loss_mv(sigma_tilde=sigma_fsopt, sigma=Sigma)
+    exp_sample = loss_mv(sigma_tilde=sigma_sample, sigma=sigma)
+    exp_sigma_tilde = loss_mv(sigma_tilde=sigma_sample, sigma=sigma)
+    exp_fsopt = loss_mv(sigma_tilde=sigma_fsopt, sigma=sigma)
 
-    prial = prial_mv(exp_sample=E_Sn, exp_sigma_tilde=E_Sigma_tilde, exp_fsopt=E_Sstar)
+    prial = prial_mv(exp_sample=exp_sample, exp_sigma_tilde=exp_sigma_tilde, exp_fsopt=exp_fsopt)
 
-    assert(prial == 0.0)
+    assert prial == 0.0
 
 
 def test_prial_fsopt():
+    '''Testing prial evaluated in finite-sample optimal covariance matrix
+    '''
     # population covariance matrix
-    Sigma = np.array([[3.00407916, -1.46190757, 1.50140806, 1.50933526, 0.27036442],
+    sigma = np.array([[3.00407916, -1.46190757, 1.50140806, 1.50933526, 0.27036442],
                       [-1.46190757, 5.61441061, -0.51939653, -2.76492235, 1.38225566],
                       [1.50140806, -0.51939653, 2.3068582, 1.41248896, 0.84740175],
                       [1.50933526, -2.76492235, 1.41248896, 6.57182938, 0.73407095],
                       [0.27036442, 1.38225566, 0.84740175, 0.73407095, 9.50282265]])
 
-    p, n = Sigma.shape[0], 3*Sigma.shape[0]
+    p_size, n_size = sigma.shape[0], 3*sigma.shape[0]
     # input data matrix
-    X = np.random.multivariate_normal(np.random.randn(p), Sigma, size=n)
+    mtx = np.random.multivariate_normal(np.random.randn(p_size), sigma, size=n_size)
 
-    sigma_sample = sample_estimator(X)
-    sigma_fsopt = fsopt_estimator(X, Sigma)
+    sigma_sample = sample_estimator(mtx)
+    sigma_fsopt = fsopt_estimator(mtx, sigma)
 
-    E_Sn = loss_mv(sigma_tilde=sigma_sample, sigma=Sigma)
-    E_Sigma_tilde = loss_mv(sigma_tilde=sigma_fsopt, sigma=Sigma)
-    E_Sstar = loss_mv(sigma_tilde=sigma_fsopt, sigma=Sigma)
+    exp_sample = loss_mv(sigma_tilde=sigma_sample, sigma=sigma)
+    exp_sigma_tilde = loss_mv(sigma_tilde=sigma_fsopt, sigma=sigma)
+    exp_fsopt = loss_mv(sigma_tilde=sigma_fsopt, sigma=sigma)
 
-    prial = prial_mv(exp_sample=E_Sn, exp_sigma_tilde=E_Sigma_tilde, exp_fsopt=E_Sstar)
+    prial = prial_mv(exp_sample=exp_sample, exp_sigma_tilde=exp_sigma_tilde, exp_fsopt=exp_fsopt)
 
-    assert(prial == 1.0)
+    assert prial == 1.0
