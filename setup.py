f702e1f7c45ad1ea066e1cc95b44707b1f1faf44
Alejandro Santorum <alejandro.santorum@gmail.com>
2021-05-09 01:15:07 +0200

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,40 @@
+# encoding: utf-8
+
+import os
+import sys
+
+from setuptools import find_packages, setup
+
+with open("README.md", "r") as f:
+    LONG_DESCRIPTION = f.read()
+
+REQUIREMENTS = read_file("requirements.txt").split("\n")
+
+setup(
+    name='scikit-rmt',
+    author='Alejandro Santorum Varela',
+    author_email='alejandro.santorum@gmail.com',
+    #packages = ['scikit-rmt'],
+    version = '0.1',
+    license='BSD',
+    description='Random Matrix Theory Python package',
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type="text/markdown",
+    url = 'https://github.com/AlejandroSantorum/scikit-rmt',
+    download_url='https://github.com/user/reponame/archive/v_01.tar.gz',
+    packages=find_packages(),
+    keywords=['RMT', 'Random Matrix Theory', 'Ensemble', 'Covariance matrices'],
+    install_requires=REQUIREMENTS,
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: BSD License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Topic :: Scientific/Engineering :: Mathematics',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+)
