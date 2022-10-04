73dc802f3f50a5fca74fc9add8f88c4eefec6413
Alejandro Santorum <alejandro.santorum@gmail.com>
2022-10-04 15:32:19 +0200

    Update setup file

--- setup.py
+++ setup.py
@@ -14,6 +14,8 @@ with open("README.md", "r") as f:
 with open("requirements.txt", "r") as f:
     REQUIREMENTS = f.read().split('\n')
 
+DOWNLOAD_URL = f'https://github.com/AlejandroSantorum/scikit-rmt/archive/refs/tags/v{VERSION}.tar.gz'
+
 setup(
     name='scikit-rmt',
     author='Alejandro Santorum Varela',
@@ -25,7 +27,7 @@ setup(
     long_description=LONG_DESCRIPTION,
     long_description_content_type="text/markdown",
     url = 'https://github.com/AlejandroSantorum/scikit-rmt',
-    download_url='https://github.com/AlejandroSantorum/scikit-rmt/archive/refs/tags/v0.2.tar.gz',
+    download_url=DOWNLOAD_URL,
     include_package_data=True,
     packages=find_packages(),
     keywords=['RMT', 'Random Matrix Theory', 'Ensemble', 'Covariance matrices'],
