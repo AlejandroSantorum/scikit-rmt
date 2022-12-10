6a2140a6173c36ae38de7d0907f49f1646d41adc
Alejandro Santorum <alejandro.santorum@gmail.com>
2022-12-10 17:23:47 +0000

    Add tests manova analytical limiting distr

--- skrmt/ensemble/tests/test_plot_law.py
+++ skrmt/ensemble/tests/test_plot_law.py
@@ -10,6 +10,7 @@ import pytest
 from skrmt.ensemble import wigner_semicircular_law
 from skrmt.ensemble import marchenko_pastur_law
 from skrmt.ensemble import tracy_widom_law
+from skrmt.ensemble import manova_spectrum_distr
 
 
 TMP_DIR_PATH = os.path.join(os.getcwd(), "skrmt/ensemble/tests/tmp")
@@ -151,6 +152,19 @@ class TestWignerSemicircleLaw:
             savefig_path=TMP_DIR_PATH+"/"+fig_name
         )
         assert os.path.isfile(os.path.join(TMP_DIR_PATH, fig_name)) == True
+    
+    def test_wscl_size_exception(self):
+        with pytest.raises(ValueError):
+            wigner_semicircular_law(
+            ensemble='goe',
+            n_size=0,
+        )
+    
+    def test_wscl_ensemble_exception(self):
+        with pytest.raises(ValueError):
+            wigner_semicircular_law(
+            ensemble='foo',
+        )
 
 
 
@@ -182,10 +196,10 @@ class TestMarchenkoPasturLaw():
         )
         assert os.path.isfile(os.path.join(TMP_DIR_PATH, fig_name)) == True
     
-    def test_wce_abs_freq(self):
-        fig_name = "test_mpl_wce_absfreq.png"
+    def test_wqe_abs_freq(self):
+        fig_name = "test_mpl_wqe_absfreq.png"
         marchenko_pastur_law(
-            ensemble='wce',
+            ensemble='wqe',
             p_size=40,
             n_size=100,
             bins=100,
@@ -221,10 +235,10 @@ class TestMarchenkoPasturLaw():
         )
         assert os.path.isfile(os.path.join(TMP_DIR_PATH, fig_name)) == True
     
-    def test_wce_normalized(self):
-        fig_name = "test_mpl_wce_norm.png"
+    def test_wqe_normalized(self):
+        fig_name = "test_mpl_wqe_norm.png"
         marchenko_pastur_law(
-            ensemble='wce',
+            ensemble='wqe',
             p_size=40,
             n_size=100,
             bins=100,
@@ -260,10 +274,10 @@ class TestMarchenkoPasturLaw():
         )
         assert os.path.isfile(os.path.join(TMP_DIR_PATH, fig_name)) == True
     
-    def test_wce_theoretical(self):
-        fig_name = "test_mpl_wce_theory.png"
+    def test_wqe_theoretical(self):
+        fig_name = "test_mpl_wqe_theory.png"
         marchenko_pastur_law(
-            ensemble='wce',
+            ensemble='wqe',
             p_size=40,
             n_size=100,
             bins=100,
@@ -286,6 +300,19 @@ class TestMarchenkoPasturLaw():
         )
         assert os.path.isfile(os.path.join(TMP_DIR_PATH, fig_name)) == True
     
+    def test_wre_theoretical_ratio_ge1(self):
+        fig_name = "test_mpl_wre_theory_ratio_ge1.png"
+        marchenko_pastur_law(
+            ensemble='wre',
+            p_size=200,
+            n_size=100,
+            bins=100,
+            density=True,
+            limit_pdf=True,
+            savefig_path=TMP_DIR_PATH+"/"+fig_name
+        )
+        assert os.path.isfile(os.path.join(TMP_DIR_PATH, fig_name)) == True
+    
     def test_wre_interval(self):
         fig_name = "test_mpl_wre_interval.png"
         marchenko_pastur_law(
@@ -299,6 +326,19 @@ class TestMarchenkoPasturLaw():
             savefig_path=TMP_DIR_PATH+"/"+fig_name
         )
         assert os.path.isfile(os.path.join(TMP_DIR_PATH, fig_name)) == True
+    
+    def test_mpl_size_exception(self):
+        with pytest.raises(ValueError):
+            marchenko_pastur_law(
+            ensemble='mre',
+            n_size=0,
+        )
+    
+    def test_mpl_ensemble_exception(self):
+        with pytest.raises(ValueError):
+            marchenko_pastur_law(
+            ensemble='foo',
+        )
 
 
 
@@ -420,3 +460,204 @@ class TestTracyWidomLaw:
             savefig_path=TMP_DIR_PATH+"/"+fig_name
         )
         assert os.path.isfile(os.path.join(TMP_DIR_PATH, fig_name)) == True
+    
+    def test_twl_size_exception(self):
+        with pytest.raises(ValueError):
+            tracy_widom_law(
+            ensemble='goe',
+            n_size=0,
+        )
+    
+    def test_twl_ensemble_exception(self):
+        with pytest.raises(ValueError):
+            tracy_widom_law(
+            ensemble='foo',
+        )
+
+
+
+class TestManovaSpectrumDistr():
+
+    def test_mre_abs_freq(self):
+        fig_name = "test_msd_mre_absfreq.png"
+        manova_spectrum_distr(
+            ensemble='mre',
+            m_size=40,
+            n1_size=100,
+            n2_size=100,
+            bins=100,
+            density=False,
+            limit_pdf=False,
+            savefig_path=TMP_DIR_PATH+"/"+fig_name
+        )
+        assert os.path.isfile(os.path.join(TMP_DIR_PATH, fig_name)) == True
+
+    def test_mce_abs_freq(self):
+        fig_name = "test_msd_mce_absfreq.png"
+        manova_spectrum_distr(
+            ensemble='mce',
+            m_size=40,
+            n1_size=100,
+            n2_size=100,
+            bins=100,
+            density=False,
+            limit_pdf=False,
+            savefig_path=TMP_DIR_PATH+"/"+fig_name
+        )
+        assert os.path.isfile(os.path.join(TMP_DIR_PATH, fig_name)) == True
+    
+    def test_mqe_abs_freq(self):
+        fig_name = "test_msd_mqe_absfreq.png"
+        manova_spectrum_distr(
+            ensemble='mqe',
+            m_size=40,
+            n1_size=100,
+            n2_size=100,
+            bins=100,
+            density=False,
+            limit_pdf=False,
+            savefig_path=TMP_DIR_PATH+"/"+fig_name
+        )
+        assert os.path.isfile(os.path.join(TMP_DIR_PATH, fig_name)) == True
+    
+    def test_mre_normalized(self):
+        fig_name = "test_msd_mre_norm.png"
+        manova_spectrum_distr(
+            ensemble='mre',
+            m_size=40,
+            n1_size=100,
+            n2_size=100,
+            bins=100,
+            density=True,
+            limit_pdf=False,
+            savefig_path=TMP_DIR_PATH+"/"+fig_name
+        )
+        assert os.path.isfile(os.path.join(TMP_DIR_PATH, fig_name)) == True
+
+    def test_mce_normalized(self):
+        fig_name = "test_msd_mce_norm.png"
+        manova_spectrum_distr(
+            ensemble='mce',
+            m_size=40,
+            n1_size=100,
+            n2_size=100,
+            bins=100,
+            density=True,
+            limit_pdf=False,
+            savefig_path=TMP_DIR_PATH+"/"+fig_name
+        )
+        assert os.path.isfile(os.path.join(TMP_DIR_PATH, fig_name)) == True
+    
+    def test_mqe_normalized(self):
+        fig_name = "test_msd_mqe_norm.png"
+        manova_spectrum_distr(
+            ensemble='mqe',
+            m_size=40,
+            n1_size=100,
+            n2_size=100,
+            bins=100,
+            density=True,
+            limit_pdf=False,
+            savefig_path=TMP_DIR_PATH+"/"+fig_name
+        )
+        assert os.path.isfile(os.path.join(TMP_DIR_PATH, fig_name)) == True
+    
+    def test_mre_theoretical(self):
+        fig_name = "test_msd_mre_theory.png"
+        manova_spectrum_distr(
+            ensemble='mre',
+            m_size=40,
+            n1_size=100,
+            n2_size=100,
+            bins=100,
+            density=True,
+            limit_pdf=True,
+            savefig_path=TMP_DIR_PATH+"/"+fig_name
+        )
+        assert os.path.isfile(os.path.join(TMP_DIR_PATH, fig_name)) == True
+
+    def test_mce_theoretical(self):
+        fig_name = "test_msd_mce_theory.png"
+        manova_spectrum_distr(
+            ensemble='mce',
+            m_size=40,
+            n1_size=100,
+            n2_size=100,
+            bins=100,
+            density=True,
+            limit_pdf=True,
+            savefig_path=TMP_DIR_PATH+"/"+fig_name
+        )
+        assert os.path.isfile(os.path.join(TMP_DIR_PATH, fig_name)) == True
+    
+    def test_mqe_theoretical(self):
+        fig_name = "test_msd_mqe_theory.png"
+        manova_spectrum_distr(
+            ensemble='mqe',
+            m_size=40,
+            n1_size=100,
+            n2_size=100,
+            bins=100,
+            density=True,
+            limit_pdf=True,
+            savefig_path=TMP_DIR_PATH+"/"+fig_name
+        )
+        assert os.path.isfile(os.path.join(TMP_DIR_PATH, fig_name)) == True
+    
+    def test_mre_ratio_ge1(self):
+        fig_name = "test_msd_mre_ratio_ge1.png"
+        manova_spectrum_distr(
+            ensemble='mre',
+            m_size=200,
+            n1_size=100,
+            n2_size=100,
+            bins=100,
+            density=False,
+            limit_pdf=False,
+            savefig_path=TMP_DIR_PATH+"/"+fig_name
+        )
+        assert os.path.isfile(os.path.join(TMP_DIR_PATH, fig_name)) == True
+    
+    def test_msd_mre_theoretical_ratio_ge1(self):
+        fig_name = "test_msd_wre_theory_ratio_ge1.png"
+        manova_spectrum_distr(
+            ensemble='mre',
+            m_size=1000,
+            n1_size=800,
+            n2_size=800,
+            bins=100,
+            density=True,
+            limit_pdf=True,
+            savefig_path=TMP_DIR_PATH+"/"+fig_name
+        )
+        assert os.path.isfile(os.path.join(TMP_DIR_PATH, fig_name)) == True
+    
+    def test_mre_interval(self):
+        fig_name = "test_msd_mre_interval.png"
+        manova_spectrum_distr(
+            ensemble='mre',
+            m_size=200,
+            n1_size=100,
+            n2_size=100,
+            bins=100,
+            interval=(0,10),
+            density=False,
+            limit_pdf=False,
+            savefig_path=TMP_DIR_PATH+"/"+fig_name
+        )
+        assert os.path.isfile(os.path.join(TMP_DIR_PATH, fig_name)) == True
+    
+    def test_msd_size_exception(self):
+        with pytest.raises(ValueError):
+            manova_spectrum_distr(
+            ensemble='mre',
+            m_size=0,
+            n1_size=0
+        )
+    
+    def test_msd_ensemble_exception(self):
+        with pytest.raises(ValueError):
+            manova_spectrum_distr(
+            ensemble='foo',
+        )
+    
