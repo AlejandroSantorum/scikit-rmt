ee67c3904ab996f6d40d9d33ff0d4968cf6ea508
Alejandro Santorum <alejandro.santorum@gmail.com>
2023-01-27 18:56:29 +0000

    Deleted VERSION file due to packaging problems

--- setup.py
+++ setup.py
@@ -1,26 +1,38 @@
 # encoding: utf-8
 
 import os
+import re
 from setuptools import find_packages, setup
 
+# Getting release version
+VERSION_FILEPATH = "skrmt/_version.py"
 with open(
-    os.path.join(os.path.dirname(__file__), "VERSION"),
+    os.path.join(os.path.dirname(__file__), VERSION_FILEPATH),
     "r",
 ) as version_file:
-    VERSION = version_file.read().strip()
+    version_file_text = version_file.read()
+    VSRE = r"^__version__ = ['\"]([^'\"]*)['\"]"
+    match = re.search(VSRE, version_file_text, re.M)
+    if match:
+        VERSION = match.group(1)
+    else:
+        raise RuntimeError(f"Unable to find version in {VERSION_FILEPATH}.")
 
+# Getting README to include as a long description
 with open(
     os.path.join(os.path.dirname(__file__), "README.md"),
     "r",
 ) as readme_file:
     LONG_DESCRIPTION = readme_file.read()
 
+# Getting packaging requirements from requirements.txt file
 with open(
     os.path.join(os.path.dirname(__file__), "requirements.txt"),
     "r",
 ) as req_file:
     REQUIREMENTS = req_file.read().split('\n')
 
+# URL to download release
 DOWNLOAD_URL = f'https://github.com/AlejandroSantorum/scikit-rmt/archive/refs/tags/v{VERSION}.tar.gz'
 
 setup(
