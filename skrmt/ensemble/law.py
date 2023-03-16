b04f22611869a8c71c9fe1105526f0c58a8ee8a5
Alejandro Santorum <alejandro.santorum@gmail.com>
2023-03-16 22:46:30 +0000

    Add MarchenkoPasturDistribution (not tested)

--- skrmt/ensemble/law.py
+++ skrmt/ensemble/law.py
@@ -56,7 +56,7 @@ def _indicator(x, start=None, stop=None, inclusive="both"):
         elif inclusive == "neither" or inclusive == "left":
             condition = (x < stop)
     
-    return np.where(condition, 1, 0)
+    return np.where(condition, 1.0, 0.0)
 
 
 
@@ -79,3 +79,66 @@ class WignerSemicircleDistribution:
     def cdf(self, x):
         return 0.5 + (x * np.sqrt(self.radius**2 - x**2))/(np.pi * self.radius**2) + \
             (np.arcsin(x/self.radius)) / np.pi
+
+
+class MarchenkoPasturDistribution:
+
+    ARCTAN_OF_INFTY = np.pi/2
+
+    def __init__(self, ratio, ensemble="wre", sigma=1.0):
+        if ratio < 0:
+            raise ValueError(f"Error: invalid ratio. It has to be positive. Provided ratio = {ratio}.")
+        try:
+            self.beta = ["wre", "wce", None, "wqe"].index(ensemble) + 1
+        except ValueError:
+            raise ValueError(f"Error: Ensemble '{ensemble}' not supported."
+                            " Check that ensemble is one of the following: 'wre', 'wce' or 'wqe'.")
+
+        self.ratio = ratio
+        self.ensemble = ensemble
+        self.sigma = sigma
+        self.lambda_minus = self.beta * self.sigma**2 * (1 - np.sqrt(self.ratio))**2
+        self.lambda_plus = self.beta * self.sigma**2 * (1 + np.sqrt(self.ratio))**2
+        self._var = self.beta * self.sigma**2
+
+    def pdf(self, x):
+        return np.sqrt(_relu(self.lambda_plus - x) * _relu(x - self.lambda_minus)) \
+            / (2.0 * np.pi * self.ratio * self._var * x)
+
+    def cdf(self, x):
+        np.seterr(divide='ignore')
+        np.seterr(invalid='ignore')
+
+        acum = _indicator(x, start=self.lambda_plus, inclusive="left")
+        acum += np.where(_indicator(x, start=self.lambda_minus, stop=self.lambda_plus, inclusive="left"),
+                         self._cdf_aux_f(x), 0.0)
+
+        if self.ratio <= 1:
+            if acum.shape == (): return float(acum)
+            return acum
+        
+        acum += np.where(_indicator(x, start=self.lambda_minus, stop=self.lambda_plus, inclusive="left"),
+                         (self.ratio-1)/(2*self.ratio), 0.0)
+        acum += np.where(_indicator(x, start=0, stop=self.lambda_minus, inclusive="left"),
+                         (self.ratio-1)/self.ratio, 0.0)
+
+        if acum.shape == (): return float(acum)
+        return acum
+
+    def _cdf_aux_f(self, x):
+        first_arctan_term = np.where(x == self.lambda_minus,
+                                    MarchenkoPasturDistribution.ARCTAN_OF_INFTY,
+                                    np.arctan((self._cdf_aux_r(x)**2 - 1)/(2 * self._cdf_aux_r(x)))
+                                )
+
+        second_arctan_term = np.where(x == self.lambda_minus,
+                                      MarchenkoPasturDistribution.ARCTAN_OF_INFTY,
+                                      np.arctan((self.lambda_minus*self._cdf_aux_r(x)**2 - self.lambda_plus) \
+                                                / (2*self._var*(1-self.ratio)*self._cdf_aux_r(x)))
+                                )
+        return 1/(2*np.pi*self.ratio) * (np.pi*self.ratio \
+                                         + (1/self._var)*np.sqrt(_relu(self.lambda_plus-x)*_relu(x-self.lambda_minus)) \
+                                         - (1+self.ratio)*first_arctan_term + (1-self.ratio)*second_arctan_term)
+
+    def _cdf_aux_r(self, x):
+        return np.sqrt((self.lambda_plus-x)/(x - self.lambda_minus))
