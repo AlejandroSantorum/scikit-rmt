80d4e9e1b6187d2ffa29a5b2e590ff879382d8d1
Alejandro Santorum <alejandro.santorum@gmail.com>
2023-05-13 17:27:31 +0100

    Update __init__.py after deprecating plot_law submodule

--- skrmt/ensemble/__init__.py
+++ skrmt/ensemble/__init__.py
@@ -13,11 +13,6 @@ from .spectral_law import MarchenkoPasturDistribution
 from .spectral_law import TracyWidomDistribution
 from .spectral_law import ManovaSpectrumDistribution
 
-from .plot_law import wigner_semicircle
-from .plot_law import marchenko_pastur
-from .plot_law import tracy_widom
-from .plot_law import manova_spectrum
-
 from .tridiagonal_utils import tridiag_eigval_neg
 from .tridiagonal_utils import tridiag_eigval_hist
 from .tridiagonal_utils import householder_reduction
@@ -29,10 +24,6 @@ __all__ = ["GaussianEnsemble", "WishartEnsemble",
            "MarchenkoPasturDistribution",
            "TracyWidomDistribution",
            "ManovaSpectrumDistribution",
-           "wigner_semicircle",
-           "marchenko_pastur",
-           "tracy_widom",
-           "manova_spectrum",
            "tridiag_eigval_neg",
            "tridiag_eigval_hist",
            "householder_reduction"
