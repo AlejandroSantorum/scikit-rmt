f2b4a1c204277aa956f56423c6f553123aac9a48
Alejandro Santorum <alejandro.santorum@gmail.com>
2023-05-13 10:59:03 +0100

    Add empirical pdf to TracyWidomDistribution class

--- skrmt/ensemble/spectral_law.py
+++ skrmt/ensemble/spectral_law.py
@@ -816,6 +816,92 @@ class TracyWidomDistribution:
             plot_ylabel="cumulative distribution", savefig_path=savefig_path
         )
 
+    def plot_empirical_pdf(self, n_size=100, times=1000, bins=100, interval=None,
+                           density=False, plot_law_pdf=False, savefig_path=None):
+        """Computes and plots Tracy-Widom empirical law using Gaussian Ensemble.
+
+        Calculates and plots Tracy-Widom empirical law using Gaussian Ensemble random matrices.
+        Because we need to obtain the largest eigenvalue of each sampled random matrix,
+        we need to sample a certain amount them. For each random matrix sammpled, its
+        largest eigenvalue is calcualted in order to simulate its density.
+
+        Args:
+            n_size (int, default=100): random matrix size n times n.
+            times (int, default=1000): number of times to sample a random matrix.
+            bins (int or sequence, default=100): If bins is an integer, it defines the number
+                of equal-width bins in the range. If bins is a sequence, it defines the
+                bin edges, including the left edge of the first bin and the right
+                edge of the last bin; in this case, bins may be unequally spaced.
+            interval (tuple, default=None): Delimiters (xmin, xmax) of the histogram.
+                The lower and upper range of the bins. Lower and upper outliers are ignored.
+            density (bool, default=False): If True, draw and return a probability
+                density: each bin will display the bin's raw count divided by the total
+                number of counts and the bin width, so that the area under the histogram
+                integrates to 1. If set to False, the absolute frequencies of the eigenvalues
+                are returned.
+            plot_law_pdf (bool, default=False): If True, the limiting theoretical law is plotted.
+                If set to False, just the empirical histogram is shown. This parameter is only
+                considered when the argument 'density' is set also to True.
+            savefig_path (string, default=None): path to save the created figure. If it is not
+                provided, the plot is shown are the end of the routine.
+
+        References:
+            - Albrecht, J. and Chan, C.P. and Edelman, A.
+                "Sturm sequences and random eigenvalue distributions".
+                Foundations of Computational Mathematics. 9.4 (2008): 461-483.
+            - Dumitriu, I. and Edelman, A.
+                "Matrix Models for Beta Ensembles".
+                Journal of Mathematical Physics. 43.11 (2002): 5830-5847.
+
+        """
+        # pylint: disable=too-many-arguments
+        if n_size<1 or times<1:
+            raise ValueError("matrix size or number of repetitions must be positive")
+
+        ens = GaussianEnsemble(beta=self.beta, n=n_size, use_tridiagonal=False)
+
+        eigvals = []
+        for _ in range(times):
+            eigvals.append(ens.eigvals().max())
+            ens.sample()
+        eigvals = np.asarray(eigvals)
+
+        # Tracy-Widom eigenvalue distr. normalization constants
+        eigval_scale = 1.0
+        size_scale = 1.0
+        if self.beta == 2:
+            eigval_scale = 1/np.sqrt(2)
+        elif self.beta == 4:
+            eigval_scale = size_scale = 1/np.sqrt(2)
+            n_size *= 2
+        eigvals = size_scale*(n_size**(1/6))*(eigval_scale*eigvals - (2.0*np.sqrt(n_size)))
+
+        if interval is None:
+            xmin=eigvals.min()
+            xmax=eigvals.max()
+            interval=(xmin, xmax)
+
+        # using numpy to obtain histogram in the given interval and no. of bins
+        observed, bins = np.histogram(eigvals, bins=bins, range=interval, density=density)
+        width = bins[1]-bins[0]
+        plt.bar(bins[:-1], observed, width=width, align='edge')
+
+        # Plotting theoretical graphic
+        if plot_law_pdf and density:
+            centers = _get_bins_centers_and_contour(bins)
+            pdf = self.pdf(centers)
+            plt.plot(centers, pdf, color='red', linewidth=2)
+
+        plt.title("Tracy-Widom Law - Empirical density histogram", fontweight="bold")
+        plt.xlabel("x")
+        plt.ylabel("probability density")
+
+        # Saving plot or showing it
+        if savefig_path:
+            plt.savefig(savefig_path, dpi=1200)
+        else:
+            plt.show()
+
 
 
 class ManovaSpectrumDistribution:
