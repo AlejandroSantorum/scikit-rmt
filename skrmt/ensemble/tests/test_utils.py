3624c8e1971fe7c4cb5dad8ff82df6b429e2fac1
Alejandro Santorum <alejandro.santorum@gmail.com>
2023-08-03 21:09:57 +0100

    Improve unit tests ensemble.utils

--- skrmt/ensemble/tests/test_utils.py
+++ skrmt/ensemble/tests/test_utils.py
@@ -10,8 +10,13 @@ from skrmt.ensemble.gaussian_ensemble import GaussianEnsemble
 def test_rand_mtx_max_eigvals():
     """Testing getting maximum eigenvalues of an Ensemble object
     """
-    goe = GaussianEnsemble(beta=1, n=10, random_state=1)
+    goe = GaussianEnsemble(beta=4, n=10, random_state=1)
 
     max_eigval = goe.eigvals().max()
-    max_vals = rand_mtx_max_eigvals(goe, size=1, random_state=1)
+
+    max_vals = rand_mtx_max_eigvals(goe, n_eigvals=1, normalize=False, random_state=1)
     assert max_eigval == max_vals[0]
+
+    max_vals_norm = rand_mtx_max_eigvals(goe, n_eigvals=1, normalize=True, random_state=1)
+    # now it has to be different since it was normalized by Tracy-Widom distr. constants
+    assert max_eigval != max_vals_norm[0]
