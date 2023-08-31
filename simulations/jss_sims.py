debb58906e67f10e4bc30630dc19c909bfa86958
Alejandro Santorum <alejandro.santorum@gmail.com>
2023-08-31 20:23:43 +0100

    Add script to generate Figure 8

--- simulations/jss_sims.py
+++ simulations/jss_sims.py
@@ -1,5 +1,6 @@
 import os
 import sys
+import numpy as np
 import matplotlib.pyplot as plt
 
 dirname = os.path.dirname(__file__)
@@ -129,10 +130,82 @@ def plot_figure_5():
 
 
 # TODO: Function for Figure 6 !!!
+# TODO: Function for Figure 7 !!!
 
 
-def plot_figure_7():
-    pass
+def plot_figure_8():
+    print("Generating images of Figure 8. This may take some few minutes...")
+
+    ### Subplot 1
+    np.random.seed(1)
+
+    t = 3_000_000
+
+    lambdas_1 = []
+    lambdas_2 = []
+    for i in range(t):
+        goe = GaussianEnsemble(n=2, beta=1, tridiagonal_form=False)
+
+        eigvals = 1/np.sqrt(2) * goe.eigvals()
+        lambdas_1.append(eigvals[0])
+        lambdas_2.append(eigvals[1])
+
+
+    ### Subplot 2
+    np.random.seed(1)
+
+    goe = GaussianEnsemble(n=2, beta=1)
+
+    t = 100
+    radius = 3
+
+    # remember, lambda_1 <= lambda_2
+    x_vals = np.linspace(-radius, radius, t)
+    y_vals = np.linspace(-radius, radius, t)
+
+    joint_pdf = []
+    for i in range(t):
+        aux_jpdf = []
+        for j in range(t):
+            # picking eigenvalues
+            v1 = x_vals[j]
+            v2 = y_vals[i]
+
+            if v2 < v1:
+                # joint eigenvalue pdf is zero since we do not consider lambda_2 < lambda_1
+                aux_jpdf.append(0.0)
+            
+            else:
+                # computing eigenvalue joint pdf
+                jepdf = goe.joint_eigval_pdf(np.asarray([v1, v2]))
+                aux_jpdf.append(jepdf)
+
+        joint_pdf.append(aux_jpdf)
+
+    joint_pdf = np.asarray(joint_pdf)
+
+    ### Plotting
+    fig, axes = plt.subplots(nrows=1, ncols=2)
+    fig.set_figheight(5)
+    fig.set_figwidth(13)
+    fig.subplots_adjust(hspace=.1)
+
+    _ = axes[0].hist2d(lambdas_1, lambdas_2, bins=30, range=[[-3,3], [-3,3]])
+    _ = axes[0].set_aspect('equal', adjustable='box')
+    _ = axes[0].set_xlabel(r"$\lambda_1$", fontsize=12)
+    _ = axes[0].set_ylabel(r"$\lambda_2$", fontsize=12)
+    _ = axes[0].set_title("Histogram of eigenvalues of GOE matrices 2x2")
+
+    img = axes[1].contourf(x_vals, y_vals, joint_pdf, extend="both")
+    _ = axes[1].set_xlabel(r"$\lambda_1$", fontsize=12)
+    _ = axes[1].set_ylabel(r"$\lambda_2$", fontsize=12)
+    _ = fig.colorbar(img, label="joint eigenvalue PDF", ax=axes)
+    _ = axes[1].set_title("Joint Eigenvalue PDF for GOE matrices 2x2")
+
+    figpath = os.path.join(SCRIPT_PATH, IMGS_DIRNAME, "fig8_goe_2x2.png")
+    plt.savefig(figpath)
+
+    print("[DONE] - Images Figure 8")
 
 
 
@@ -146,8 +219,9 @@ def main():
     # plot_figure_5()
 
     # TODO: Figure 6 !!!!
+    # TODO: Figure 7 !!!!
 
-    plot_figure_7()
+    plot_figure_8()
 
 
 
